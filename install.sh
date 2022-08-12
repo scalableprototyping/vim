cbee296d4273d4cb869bd8f6bd8b5c3dd1a4c81b
blasco <blascoburguillos@gmail.com>
2022-08-12 10:10:18 +0200

    changed gitpod install folder to .dotfiles

--- install.sh
+++ install.sh
@@ -1,23 +1,22 @@
 # Gitpod install script
 pwd
-cp -r ~/vim ~/dotfiles_vim
 
 # Vim
 sudo add-apt-repository -y ppa:jonathonf/vim
 sudo apt-get update
 sudo apt-get install -y vim
 rm -rf ~/.vim
-sudo ln -s ~/dotfiles_vim ~/.vim
+sudo ln -s ~/.dotfiles ~/.vim
 vim +PlugInstall +qall
-touch ~/dotfiles_vim/.vim_initialized
+touch ~/.dotfiles/.vim_initialized
 
 # Nvim
 mkdir -p ~/programs/neovim
 cd ~/programs/neovim
 wget https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.tar.gz
 tar xzvf nvim-linux64.tar.gz
-sudo ln -s ~/programs/neovim/nvim-linux64/bin /usr/bin/
+sudo ln -s ~/programs/neovim/nvim-linux64/bin/nvim /usr/bin/
 rm nvim-linux64.tar.gz
 mkdir ~/.config
-ln -s ~/dotfiles_vim  ~/.config/nvim
+ln -s ~/.dotfiles  ~/.config/nvim
 nvim +PlugInstall +qall

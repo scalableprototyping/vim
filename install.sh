02db4d79df83daf2c5bd1913ceec0260b8cb94b0
blasco <blascoburguillos@gmail.com>
2022-08-19 17:13:05 +0000

    wip

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Gitpod install script
 
 # # Vim
@@ -13,9 +15,11 @@ mkdir -p ~/programs/neovim
 cd ~/programs/neovim
 wget https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.tar.gz
 tar xzvf nvim-linux64.tar.gz
-sudo rm /usr/bin/nvim
+if [ -f "/usr/bin/nvim" ] ; then
+    sudo rm "/usr/bin/nvim"
+fi
 sudo ln -s ~/programs/neovim/nvim-linux64/bin/nvim /usr/bin/
-rm nvim-linux64.tar.gz
+rm -f nvim-linux64.tar.gz
 mkdir -p ~/.config
 ln -s ~/.dotfiles  ~/.config/nvim
 nvim +PlugInstall +qall

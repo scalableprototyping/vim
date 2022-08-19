78d12a29367ff47ecbde0d077fd60c670d9ba1e9
blasco <blascoburguillos@gmail.com>
2022-08-19 17:14:56 +0000

    wip

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@
 # sudo add-apt-repository -y ppa:jonathonf/vim
 # sudo apt-get update
 # sudo apt-get install -y vim
-# rm -rf ~/.vim
-# sudo ln -s ~/.dotfiles ~/.vim
+rm -rf ~/.vim
+sudo ln -s ~/.dotfiles ~/.vim
 # vim +PlugInstall +qall
 
 # Nvim

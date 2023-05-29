3faa0af73b7b87f1906e6e4a58f699599a54ce55
blasco <blascoburguillos@gmail.com>
2023-05-29 09:59:36 +0200

    wip

--- lua/plugin_config.lua
+++ lua/plugin_config.lua
@@ -1,13 +1,13 @@
-require("plug_config.yanky-nvim")
-require("plug_config.hop")
-
 if not vim.g.vscode then
-    require("plug_config.telescope")
-    require("plug_config.gitsigns-nvim")
-    require("plug_config.lspsaga-nvim")
-    require("plug_config.nvim-lspconfig")
-    require("plug_config.nvim-tree")
-    require("plug_config.whick-key-nvim")
-    require("plug_config.nvim-cmp")
-    require("plug_config.mason")
+  require("plug_config.yanky-nvim")
+  require("plug_config.telescope")
+  require("plug_config.gitsigns-nvim")
+  require("plug_config.lspsaga-nvim")
+  require("plug_config.nvim-lspconfig")
+  require("plug_config.nvim-tree")
+  require("plug_config.whick-key-nvim")
+  require("plug_config.nvim-cmp")
+  require("plug_config.mason")
+else
+  require("plug_config.hop")
 end
